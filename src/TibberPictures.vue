46e3e077de458c43299e340ef815e5b5bc5e357f
JørgenVikanEidsvåg <jorgenvikan@gmail.com>
2022-09-23 09:15:41 +0200

    dynamic pictures

--- src/TibberPictures.vue
+++ src/TibberPictures.vue
@@ -2,15 +2,15 @@
   <div class="container text-left">
     <div class="row">
       <div class="col-lg-6">
-        <img src="./assets/img1.png" alt="img1.png">
+        <img class="img-fluid" src="./assets/imgaaa.png" alt="img1.png">
       </div>
       <div class="col-lg-3">
-        <img src="./assets/imgs.png" alt="imgs.png">
+        <img class="img-fluid" src="./assets/imgs.png" alt="imgs.png">
         <br/>
         <p class="h3"><strong>Direct mailing strategy buzz social proof</strong></p>
       </div>
       <div class="col-lg-2 ">
-        <img src="./assets/charge.png" alt="charge.png">
+        <img class="img-fluid" src="./assets/charge.png" alt="charge.png">
         <br/>
         <p class="h3"><strong>Hypotheses value proposition</strong></p>
       </div>
